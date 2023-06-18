d338ac9ee361499f79c531bf0a93c8330962c130
Ayan Bhunia <94762854+ayanbhunia2002@users.noreply.github.com>
2023-06-18 12:36:19 +0530

    Create Maximum Subarray Sum.java

---
+++ Maximum Subarray Sum.java
@@ -0,0 +1,23 @@
+import java.util.* ;
+import java.io.*; 
+
+public class Solution {
+	
+	public static long maxSubarraySum(int[] arr, int n) {
+		long cur_sum = arr[0];
+		long max_sum = 0;
+
+		for(int i = 1; i < n; i++){
+			cur_sum = cur_sum + arr[i];
+			if(cur_sum > max_sum){
+				max_sum = cur_sum;
+			}
+			if(cur_sum < 0){
+				cur_sum = 0;
+			}
+		}
+
+		return max_sum;
+	}
+
+}
